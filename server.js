b4a31c7666a17f2220ed2a4f20c1d238fcea520b
Valdemar Arantes Neto <varantes@users.noreply.github.com>
2023-08-12 12:35:56 -0300

    Inclusão de documentação OpenAPI (Swagger)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const winston = require('winston');
 const correlationId = require('express-correlation-id');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
@@ -9,7 +11,7 @@ const app = express();
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    defaultMeta: {service: 'user-service'},
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
@@ -38,25 +40,110 @@ const morganJsonFormat = (tokens, req, res) => {
 };
 app.use(morgan(morganJsonFormat));
 
+// Defina as opções para o swagger-jsdoc
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Nome da API',
+            version: '1.0.0',
+      description: 'Descrição da API'
+    }
+    },
+    // Caminho para os arquivos com as anotações dos endpoints
+  apis: ['./*.js']
+};
+
+// Crie o objeto swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// Rota para a documentação gerada pelo Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /hello-world:
+ *   get:
+ *     summary: Retorna uma mensagem de "Olá, Mundo!"
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Olá, Mundo!
+ */
 app.get('/hello-world', (req, res) => {
     console.log(`request recebido: req.url = ${req.url}`);
     res.send('Olá, Mundo!');
 });
 
+/**
+ * @swagger
+ * /v2/hello-world:
+ *   get:
+ *     summary: Retorna uma mensagem de "Olá, Mundo v2!"
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Olá, Mundo v2!
+ */
 app.get('/v2/hello-world', (req, res) => {
     res.send('Olá, Mundo v2!');
 });
 
+/**
+ * @swagger
+ * /v1/winston-test:
+ *   get:
+ *     summary: Teste do Winston
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Requisição recebida com sucesso
+ */
 app.get('/v1/winston-test', (req, res) => {
     const requestTime = new Date().toISOString();
     const correlationId = req.correlationId();
     logger.info(`[correlation-id: ${correlationId}] Winston test log at ${requestTime}`);
     res.send(`Requisição recebida em: ${requestTime}`);
 });
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
 
+/**
+ * @swagger
+ * /v2/winston-test:
+ *   get:
+ *     summary: Teste do Winston v2
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Requisição recebida com sucesso
+ */
 app.get('/v2/winston-test', (req, res) => {
     const requestTime = new Date().toISOString();
     const correlationId = req.correlationId();
@@ -68,4 +155,8 @@ app.get('/v2/winston-test', (req, res) => {
     });
 
     res.send(`Requisição recebida em: ${requestTime}`);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });

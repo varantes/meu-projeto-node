06034aa5c74a88c86b3e691d0738bb191e37630c
Valdemar Arantes Neto <142926+varantes@users.noreply.github.com>
2023-08-27 10:51:48 -0300

    O start utiliza o instrumentation-without-metrics.js, que agora está mais parecido com o instrumentation-gcp.js

--- instrumentation-without-metrics.js
+++ instrumentation-without-metrics.js
@@ -1,8 +1,13 @@
+"use strict";
+
+// [START opentelemetry_trace_import]
 const opentelemetry = require("@opentelemetry/api");
 const {NodeTracerProvider} = require("@opentelemetry/sdk-trace-node");
 const {SimpleSpanProcessor} = require("@opentelemetry/sdk-trace-base");
 const {TraceExporter,} = require("@google-cloud/opentelemetry-cloud-trace-exporter");
+// [END opentelemetry_trace_import]
 
+// [START setup_exporter]
 // Enable OpenTelemetry exporters to export traces to Google Cloud Trace.
 // Exporters use Application Default Credentials (ADCs) to authenticate.
 // See https://developers.google.com/identity/protocols/application-default-credentials
@@ -15,3 +20,37 @@ const exporter = new TraceExporter();
 
 // Configure the span processor to send spans to the exporter
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+// [END setup_exporter]
+
+// [START opentelemetry_trace_custom_span]
+function tracerTest() {
+    console.log("Start recording test traces.");
+
+    // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
+    provider.register()
+    const tracer = opentelemetry.trace.getTracer("basic");
+
+    // Create a span.
+    const span = tracer.startSpan("tracerTest");
+
+    // Set attributes to the span.
+    span.setAttribute("test-span-attribute-key", "test-span-attribute-value");
+
+    // Annotate our span to capture metadata about our operation
+    span.addEvent("invoking work of tracerTest");
+
+    for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+      // simulate some random work.
+    }
+
+    // Be sure to end the span.
+    span.end();
+    // [END opentelemetry_trace_custom_span]
+
+    console.log("Done recording test traces.");
+}
+
+tracerTest();
+
+// [END opentelemetry_trace_samples]
